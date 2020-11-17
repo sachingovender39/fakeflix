0683f65b12f4bb628612ee74627fc8c3077c1f4f
Sachin Govender <sachingovender39@gmail.com>
2020-11-17 11:17:35 +0200

    Added basic trailer functionality

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+
 function Card (props){
     const posterPath = 'http://image.tmdb.org/t/p/w200'+props.info.poster_path;
-    return <div className='ItemCard'>
+    function getTrailer(){
+        props.click('name' in props.info ? props.info.name : props.info.title);
+    }
+    return <div className='ItemCard' onClick={getTrailer}>
      <img src={posterPath} alt={'name' in props.info ? props.info.name : props.info.title}></img>
     </div>
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import axios from './base.js'
 import Card from './Card'
+import Youtube from 'react-youtube'
+import movie_trailer from 'movie-trailer'
 
 function List(props){
 
     const [items,setItems]=React.useState([]);
+    const [trailer,setTrailer] = React.useState('');
     React.useEffect(() => {
         async function fetchData(){
             const req = await axios.get(props.fetchUrl);
@@ -14,11 +17,35 @@ function List(props){
         fetchData();
     },[props.fetchUrl]);
 
+    const opts = {
+        height:'400',
+        width:'100%',
+        playerVars:{autoplay:1}
+    }
+
+    function Trailer(name){
+        if (trailer){
+            setTrailer('');
+        }
+        else{
+            movie_trailer(name || '').then(
+                (url) =>{
+                    const params = new URLSearchParams(new URL(url).search);
+                    setTrailer(params.get('v'));
+                }
+            )
+            .catch((error) => console.log(error));
+        }
+
+
+    }
+
     return <div className='ItemRow'>
         <h1>{props.title}</h1>
             <div className='items'>
-            {items.map(item => <Card info={item} key={item.id} />)}
+            {items.map(item => <Card info={item} key={item.id} click={Trailer} />)}
             </div>
+        <Youtube videoId={trailer} opts={opts} />
     </div>
 
 }
